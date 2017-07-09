6c4c710444d05ec3c2d6bb7be570013f71c8420c
zahidcserana <zahid1004048@gmail.com>
2017-07-09 23:21:29 +0600

    purchase

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DateTime;
 use Illuminate\Http\Request;
 use DB;
 
@@ -9,12 +9,66 @@ class PurchaseController extends Controller
 {
     public function Index()
     {
-    	return view('purchase_form');
+    	$voucher = '';
+    	$total_amount = '';
+    	$data['voucher'] = $voucher;
+    	$data['total_amount'] = $total_amount;
+    	return view('purchase_form',$data);
     }
     public function Purchase(Request $request)
     {
-    	$input = $request->except(['_token']);
-    	DB::table('purchases')->insertGetId($input);
+    	$vouchar_no = '';
+    	$total_amount = '';
+
+    	if (isset($_POST['search'])) {
+    		$vouchar_no = $_POST['voucher'];
+    		$total_amount = DB::table('purchases')->where('vouchar_no',$vouchar_no)->first()->amount_t;
+
+	    	$data['voucher'] = $vouchar_no;
+	    	$data['total_amount'] = $total_amount;
+
+	    	return view('purchase_form',$data);
+    	}
+    	$product_name = $request->input('product_name');
+    	$vouchar_no = $request->input('vouchar_no');
+    	$created_at = $request->input('created_at');
+    	$brand = $request->input('brand');
+    	$purchase_from = $request->input('purchase_from');
+    	$quantity = $request->input('quantity');
+    	$amount = $request->input('amount');
+    	$other_expense = $request->input('other_expense');
+    	$unit_price = $request->input('unit_price');
+    	$purchase_by = $request->input('purchase_by');
+
+		$fromFormat = 'm/j/Y';
+        $toFormat = 'Y-m-d';
+        $format = $fromFormat;
+        $dateTimeObj = DateTime::createFromFormat ( $format , $created_at);
+        $created_at = $dateTimeObj->format($toFormat);
+        $amount_t = 0;
+        for ($i=0; $i <count($product_name) ; $i++) { 
+    		$amount_t+= ($quantity[$i]*$unit_price[$i])+$other_expense[$i];
+    	}
+    	for ($i=0; $i <count($product_name) ; $i++) { 
+
+    		$input = array(
+    						'vouchar_no' 		=> $vouchar_no,
+					    	'created_at' 		=> $created_at,
+					    	'product_name' 		=> $product_name[$i],
+					    	'brand' 			=> $brand[$i],
+					    	'purchase_from' 	=> $purchase_from[$i],
+					    	'quantity' 			=> $quantity[$i],
+					    	'amount' 			=> $amount[$i],
+					    	'other_expense' 	=> $other_expense[$i],
+					    	'unit_price' 		=> $unit_price[$i],
+					    	'purchase_by' 		=> $purchase_by[$i],
+					    	'amount_t' 			=> $amount_t
+					   );
+
+			DB::table('purchases')->insertGetId($input);
+    	}
+
+    	
     	return redirect()->route('purchase');
     }
 }
--- resources/views/purchase_form.blade.php
+++ resources/views/purchase_form.blade.php
@@ -13,56 +13,67 @@
                         {{ csrf_field() }}
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label"> Vouchar No</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-4">
                                 <input type="text" name="vouchar_no" class="form-control" placeholder="Enter Vouchar No">
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Product Name</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="product_name" class="form-control" placeholder="Product Name">
+                                <label class="col-sm-2 col-form-label"> Purchase Date</label>
+                                <div class="col-sm-4">
+                                <div class="input-group date" data-provide="datepicker">
+                                    <input type="text" name="created_at" class="form-control">
+                                    <div class="input-group-addon">
+                                        <span class="glyphicon glyphicon-th"></span>
+                                    </div>
+                                </div>
                                 </div>
                             </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Brand</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="brand" class="form-control" placeholder="Brand">
+                            <div id="purchase_div">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Product Name</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="product_name[]" class="form-control" placeholder="Product Name">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Purchase From</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="purchase_from" class="form-control" placeholder="Purchase From">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Brand</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="brand[]" class="form-control" placeholder="Brand">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label"> Quantity</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="quantity" class="form-control" placeholder="Quantity">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Purchase From</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="purchase_from[]" class="form-control" placeholder="Purchase From">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Amount</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="amount" class="form-control" placeholder="Amount">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label"> Quantity</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="quantity[]" id="quantity" class="form-control" placeholder="Quantity">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Other Expense</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="other_expense" class="form-control" placeholder="Other Expense">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Amount</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="amount[]" id="amount" class="form-control" placeholder="Amount">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Unit Price</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="unit_price" class="form-control" placeholder="Unit Price">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Other Expense</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="other_expense[]" id="other_expense" class="form-control" placeholder="Other Expense">
+                                    </div>
                                 </div>
-                            </div>
-                             <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Purchase By</label>
-                                <div class="col-sm-10">
-                                <input type="text" name="purchase_by" class="form-control" placeholder="Purchase By">
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Unit Price</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="unit_price[]" id="unit_price" class="form-control" placeholder="Unit Price">
+                                    </div>
+                                </div>
+                                 <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Purchase By</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" name="purchase_by[]" class="form-control" placeholder="Purchase By">
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group row">
@@ -73,9 +84,129 @@
                         </form>
                         <!-- <a href="{{route('purchase')}}"><button class="btn btn-default">Cancel</button></a> -->
                     </div>
+                    <div class="col-md-3" >
+                        <button id="addMore" class="fa fa-plus btn blue">More</button><button id="removeDiv" class="fa fa-minus btn red">Less</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div>
+        <form method="post" action="{{route('purchase')}}" role="form">
+            {{ csrf_field() }}
+             <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Voucher No</label>
+                <div class="col-sm-4">
+                <input type="text" name="voucher" id="voucher" class="form-control" value="{{$voucher}}">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Total Amount</label>
+                <div class="col-sm-4">
+                <input type="text" name="total_amount" id="total_amount" class="form-control" value="{{$total_amount}}">
+                </div>
+            </div>
+             <div class="form-group row">
+                <div align="center" class="offset-sm-2 col-sm-10">
+                    <button type="submit" name="search" class="btn btn-primary">Search</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <input type="hidden" id="div_no">
 </div>
+
 @endsection
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="{{asset('assets/datepicker/js/bootstrap-datepicker.min.js')}}"></script>
+
+<link rel="stylesheet" href="{{asset('assets/datepicker/css/bootstrap-datepicker.min.css')}}" />
+
+<script>
+    $(function() {
+    var divId = 0; 
+    $("#addMore").click(function(e) {
+        divId++;
+        e.preventDefault();
+       $('#div_no').val(divId);
+        var items = '';
+        items+='<div class="newDiv_' + divId +'">';
+        items+='<h3><u>Another Product</u></h3>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Product Name</label><div class="col-sm-10"><input type="text" name="product_name[]" class="form-control" placeholder="Product Name"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Brand</label><div class="col-sm-10"><input type="text" name="brand[]" class="form-control" placeholder="Brand"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Purchase From</label><div class="col-sm-10"><input type="text" name="purchase_from[]" class="form-control" placeholder="Purchase From"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label"> Quantity</label><div class="col-sm-10"><input type="text" name="quantity[]" id="quantity_' + divId +'" class="form-control" placeholder="Quantity"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Amount</label><div class="col-sm-10"><input type="text" name="amount[]" id="amount_' + divId +'" class="form-control" placeholder="Amount"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Other Expense</label><div class="col-sm-10"><input type="text" name="other_expense[]" id="other_expense_' + divId +'" class="form-control" placeholder="Other Expense"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Unit Price</label><div class="col-sm-10"><input type="text" name="unit_price[]" id="unit_price_' + divId +'" class="form-control" placeholder="Unit Price"></div></div>';
+        items+='<div class="form-group row"><label class="col-sm-2 col-form-label">Purchase By</label><div class="col-sm-10"><input type="text" name="purchase_by[]" class="form-control" placeholder="Purchase By"></div></div>';
+
+        items+='</div>';
+       
+        
+        $("#purchase_div").append(items);
+    });
+
+     $("#removeDiv").click(function(e) {
+        e.preventDefault();
+        var div_id = 'newDiv_' + divId;
+        if (divId>0) {
+            $( "div" ).remove('.'+ div_id );
+           
+            divId--;
+        }
+       
+    }); 
+
+
+
+});
+    function TotalAmount()
+    {
+        var quantity = $('#quantity').val();
+        var unit_price = $('#unit_price').val();
+        var other_expense = $('#other_expense').val();
+        var amount = parseInt(quantity)*parseInt(unit_price) +  parseInt(other_expense);
+        $('#amount').val(amount);
+        
+        var totalAmount = amount;
+        var divId = $('#div_no').val();
+
+        for (var i = 1; i <= divId; i++) {
+            var quantity = $('#quantity_'+divId).val();
+            var unit_price = $('#unit_price_'+divId).val();
+            var other_expense = $('#other_expense_'+divId).val();
+            var amount = parseInt(quantity)*parseInt(unit_price) +  parseInt(other_expense);
+            totalAmount+=amount;
+            $('#amount_'+divId).val(amount);
+        }
+        $('#total_amount').val(totalAmount);
+    }
+$('.datepicker').datepicker({
+    format: 'mm/dd/yyyy',
+    startDate: '-3d'
+});
+function Amount()
+    {
+        var quantity = $('#quantity').val();
+        var unit_price = $('#unit_price').val();
+        var other_expense = $('#other_expense').val();
+        var amount = parseInt(quantity)*parseInt(unit_price) +  parseInt(other_expense);
+        $('#amount').val(amount);
+       
+        var totalAmount = amount;
+ /*
+        for (var i = 1; i <= divId; i++) {
+            var quantity = $('#quantity_'+divId).val();
+            var unit_price = $('#unit_price_'+divId).val();
+            var other_expense = $('#other_expense_'+divId).val();
+            var amount = parseInt(quantity)*parseInt(unit_price) +  parseInt(other_expense);
+            totalAmount+=amount;
+        }*/
+        $('#total_amount').val(totalAmount);
+    }
+
+</script>
